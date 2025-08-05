03b73f7a93315bdd5c06da0d0b3052edd59f6635
Ayush Martin Sabu <ayushmartin06@gmail.com>
2025-08-05 12:22:37 +0530

    fix:trainer-assignment-submission

--- src/config/ratelimiter.ts
+++ src/config/ratelimiter.ts
@@ -2,7 +2,7 @@ import rateLimit from "express-rate-limit";
 
 const limiter = rateLimit({
   windowMs: 1 * 60 * 1000,
-  max: 20,
+  max: 100,
   message: {
     success: false,
     message: "Too many requests. Please try again later.",
