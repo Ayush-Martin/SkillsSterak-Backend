7c0131749f18d0ab9c8ac4eb11e86f2e9de7f852
Ayush Martin Sabu <ayushmartin06@gmail.com>
2025-08-05 11:38:39 +0530

    added rate limited

--- src/app.ts
+++ src/app.ts
@@ -7,24 +7,25 @@ import nocache from "nocache";
 import router from "./routes/index.router";
 import webhooksRouter from "./routes/webhooks.router";
 import dotenv from "dotenv";
+import limiter from "./config/ratelimiter";
 
 dotenv.config();
 
 const app = express();
 
+app.use(morgan("dev"));
 app.use("/webhooks", webhooksRouter);
 
 app.use(cookieParser());
-app.use(morgan("dev"));
 app.use(nocache());
 app.use(cors);
+app.use(limiter);
 
 app.get("/", (req, res) => {
-    res.send("Server is running");
+  res.send("Server is running");
 });
 
 app.use("/api", express.json(), express.urlencoded({ extended: true }), router);
-
 
 app.use(errorHandler);
 
